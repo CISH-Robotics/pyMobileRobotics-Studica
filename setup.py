0ad3fa67db89d44047684c715bc199d72fa64bd4
HFDTeam_HAO <31580253+iqv29@users.noreply.github.com>
2021-07-01 03:22:26 +0800

    新增TitanQuad

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(name='pyMobileRobotics-Studica',
+      version='0.0.1',
+      description='pyMobileRobotics Studica Robot Toolkit',
+      author='CISH Robotics',
+      author_email='crt@cish.xyz',
+      url='https://github.com/CISH-Robotics',
+      keywords='robot worldskills mobile robotics studica',
+      project_urls={
+            'Source': 'https://github.com/CISH-Robotics/pyMobileRobotics-Studica',
+      },
+      packages=find_packages(),
+      install_requires=[
+            'pyMobileRobotics>=0.0.5'
+            ],
+      python_requires='>=3'
+     )
